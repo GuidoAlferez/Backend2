a383df8264bcb36aadf07ff49e441a8f195e5df4
Guido <alferezguido@outlook.com>
2021-01-30 19:49:07 -0300

    3er

--- index.js
+++ index.js
@@ -21,44 +21,44 @@ app.set('true proxy', true)
 //GET Serie
 app.get('/:serie', (req, res) => {
     SerieBuscada.findOne({name : req.params.serie})
-        .then((SerieX)=>{
-            if(SerieX){
-                res.status(200).send(SerieX)
-                const newRequest = new RequestLog({
-                    date: new Date(),
-                    search: req.params.serie,
-                    ip: req.header('x-forwarded-for') || req.connection.remoteAddress,
-                    responseFrom: "CACHE"
-                })
-            }else{
-                fetch(`${URL_DB}${req.params.serie}`)
-                .then((res)=>{return res.json()})
-                .then((json)=>{
-                    if(!json){return res.status(404).send({"Not Founded":"404 Serie Not Founded"})}
-                    else{
-                        res.status(200).send(json)
-                        const newSerie = new SerieBuscada({
-                            name:json.name,
-                            image:{medium:json.image.medium},
-                            summary:json.summary,
-                            officialSite:json.officialSite
-                           })
-                           newSerie.save().then((serieSaved)=>{
-                               return console.log(serieSaved)
-                            }).catch(err=>{ return console.log({"Error guardando serie":err})})
-                            const newRequest = new RequestLog({
-                                date: new Date(),
-                                search: req.params.serie,
-                                ip: req.header('x-forwarded-for') || req.connection.remoteAddress,
-                                responseFrom:"API"
-                            })
-                            newRequest.save().then((requestSaved) => {
-                                console.log(requestSaved)
-                            }).catch(err => { return console.log({ "Error": err }) })
-                    }
-                })
-            }
-        })
+    .then((SerieX)=>{
+        if(SerieX){
+            res.status(200).send(SerieX)
+            const newRequest = new RequestLog({
+            date: new Date(),
+            search: req.params.serie,
+            ip: req.header('x-forwarded-for') || req.connection.remoteAddress,
+            responseFrom: "CACHE"
+            })
+        }else{
+            fetch(`${URL_DB}${req.params.serie}`)
+            .then((res)=>{return res.json()})
+            .then((json)=>{
+                if(!json){return res.status(404).send({"Not Founded":"404 Serie Not Founded"})}
+                else{
+                    res.status(200).send(json)
+                    const newSerie = new SerieBuscada({
+                        name:json.name,
+                        image:{medium:json.image.medium},
+                        summary:json.summary,
+                        officialSite:json.officialSite
+                    })
+                    newSerie.save().then((serieSaved)=>{
+                        return console.log(serieSaved)
+                    }).catch(err=>{ return console.log({"Error guardando serie":err})})
+                    const newRequest = new RequestLog({
+                        date: new Date(),
+                        search: req.params.serie,
+                        ip: req.header('x-forwarded-for') || req.connection.remoteAddress,
+                        responseFrom:"API"
+                    })
+                    newRequest.save().then((requestSaved) => {
+                        console.log(requestSaved)
+                    }).catch(err => { return console.log({ "Error": err }) })
+                }
+            })
+        }
+    })
 })
 
 mongoose.connect(connectionString, ((err) => {
